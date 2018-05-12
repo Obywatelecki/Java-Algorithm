304a725e56ea255d462e95ee86aa6621e65fd51d
Obywatelecki <tpanecki@gmail.com>
2018-05-12 16:15:59 +0200

    initial commit

--- src/org/Obywatelecki/SLL.java
+++ src/org/Obywatelecki/SLL.java
@@ -2,10 +2,14 @@ package org.Obywatelecki;
 
 public class SLL<T> {
 
-    class Node<T> {
+    public class Node<T> {
 
         T data;
         Node next;
+
+        public T getData() {
+            return data;
+        }
 
         public Node(T data) {
             this.data = data;
@@ -31,7 +35,74 @@ public class SLL<T> {
         return cnt;
     }
 
-    public void append() {
+    public void append(T newData) {
+        Node ref = head;
+        Node<T> newNode = new Node<>(newData);
+
+        if (ref == null) {
+            head = newNode;
+        } else {
+            while (ref.next != null) {
+                ref = ref.next;
+            }
+            ref.next = newNode;
+        }
 
     }
+
+    public Node find(T data) {
+        Node ref = head;
+        while (ref != null) {
+            if (ref.data.equals(data)) {
+                return ref;
+            }
+            ref = ref.next;
+        }
+        return null;
+    }
+
+    public Node findNth(int i) {
+        Node ref = head;
+        int cnt = 0;
+        while (ref != null) {
+            if (cnt == i) {
+                return ref;
+            }
+            cnt++;
+            ref = ref.next;
+        }
+        return null;
+    }
+
+    public void insert(Node item, T newData) {
+        Node<T> newNode = new Node<>(newData);
+
+        if (item == null) {
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+
+        newNode.next = item.next;
+        item.next = newNode;
+
+    }
+
+    public void delete(Node item) {
+        if (head == null) {
+            return;
+        }
+
+        if (item == null) {
+            head = head.next;
+            return;
+        }
+
+        if (item.next == null) {
+            return;
+        }
+
+        item.next = item.next.next;
+    }
 }
+
