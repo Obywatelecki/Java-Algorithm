c49d3f6ca508d658851945bb3dac86fc7c0ada85
Obywatelecki <tpanecki@gmail.com>
2018-06-21 18:07:13 +0200

    initial commit

--- src/org/Obywatelecki/BinaryTree.java
+++ src/org/Obywatelecki/BinaryTree.java
@@ -16,5 +16,86 @@ public class BinaryTree {
 
     Node root;
 
+    private Node addHelper(Node node, int value) {
+        if (node == null) {
+            return new Node(value);
+        }
+
+        if (value < node.data) {
+            node.left = addHelper(node.left, value);
+        } else if (value > node.data) {
+            node.right = addHelper(node.right, value);
+        } else {
+            // value already exists
+            return node;
+        }
+
+        return node;
+    }
+
+    public void add(int data) {
+        root = addHelper(root, data);
+    }
+
+    private boolean containsHelper(Node current, int value) {
+        if (current == null) {
+            return false;
+        }
+        if (value == current.data) {
+            return true;
+        }
+        if (value < current.data) {
+            return containsHelper(current.left, value);
+        } else {
+            return containsHelper(current.right, value);
+        }
+    }
+
+    public boolean contains(int data) {
+        return containsHelper(root, data);
+    }
+
+    private int findSmmalest(Node root) {
+        if (root.left == null) {
+            return root.data;
+        } else {
+            return findSmmalest(root.left);
+        }
+    }
+
+    private Node deleteHelper(Node current, int value) {
+        if (current == null) {
+            return null;
+        }
+
+        if (value == current.data) {
+            if (current.left == null && current.right == null) {
+                return null;
+            }
+            if (current.left == null) {
+                return current.left;
+            }
+            if (current.right == null) {
+                return current.right;
+            } else {
+                int smallestValue = findSmmalest(current.right);
+                current.data= smallestValue;
+                current.right = deleteHelper(current.right, smallestValue);
+                return current;
+            }
+
+        }
+        if (value < current.data) {
+            current.left = deleteHelper(current.left, value);
+            return current;
+        }
+        current.right = deleteHelper(current.right, value);
+        return current;
+    }
+
+    public void delete(int data) {
+        deleteHelper(root, data);
+    }
+
 
 }
--- src/test/org/Obywatelecki/BinaryTreeTest.java
+++ src/test/org/Obywatelecki/BinaryTreeTest.java
@@ -1,25 +1,78 @@
-package test.org.Obywatelecki; 
+package test.org.Obywatelecki;
 
-import org.junit.Test; 
-import org.junit.Before; 
-import org.junit.After; 
+import org.Obywatelecki.BinaryTree;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 
-/** 
-* BinaryTree Tester. 
-* 
-* @author <Authors name> 
-* @since <pre>cze 21, 2018</pre> 
-* @version 1.0 
-*/ 
-public class BinaryTreeTest { 
+/**
+ * BinaryTree Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>cze 21, 2018</pre>
+ */
+public class BinaryTreeTest {
 
-@Before
-public void before() throws Exception { 
-} 
+    @Before
+    public void before() throws Exception {
+    }
 
-@After
-public void after() throws Exception { 
-} 
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: add(int data)
+     */
+    @Test
+    public void testAdd() throws Exception {
+
+        BinaryTree bt = new BinaryTree();
+        bt.add(3);
+        bt.add(4);
+        bt.add(2);
+
+    }
 
 
-} 
+    /**
+     * Method: addHelper(Node current, int value)
+     */
+
+    @Test
+    public void contains() {
+
+        BinaryTree bt = new BinaryTree();
+        bt.add(3);
+        bt.add(4);
+        bt.add(2);
+
+        Assert.assertTrue(bt.contains(3));
+        Assert.assertTrue(bt.contains(4));
+        Assert.assertFalse(bt.contains(10));
+
+    }
+
+    @Test
+    public void delete() {
+
+        BinaryTree bt = new BinaryTree();
+        bt.add(3);
+        bt.add(4);
+        bt.add(2);
+        bt.add(5);
+        bt.add(7);
+        bt.add(9);
+        bt.add(11);
+
+        bt.delete(3);
+        bt.delete(11);
+        bt.delete(4);
+
+        Assert.assertFalse(bt.contains(3));
+        Assert.assertFalse(bt.contains(11));
+        Assert.assertFalse(bt.contains(4));
+    }
+}
